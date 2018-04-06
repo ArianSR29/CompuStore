77403399a19cea96f346c749fe01e072b47223d9
ArianSR29 <green-m-an@hotmail.com>
2018-04-06 02:56:56 -0500

    completo

--- actions/iniciar_sesion.php
+++ actions/iniciar_sesion.php
@@ -1,34 +1,25 @@
 <?php
 session_start();
+
 include 'conexion_mysqli.php';
 $mail = $_POST['tb-mail'];
 $pass = $_POST['tb-password'];
-if((!$mail) || (!$pass)){
-    print "<script>alert('Usuario o Contraseña Incorrectos');</script>";
-    print "<script> window.location='../index.php';</script>";
-}else{
-    //$password = md5($pass); 
-    print "<script>alert('1')</script>";  
-    $sql = mysqli_query("SELECT email, password FROM user WHERE email = '$mail' AND password = '$pass'");
-    print "<script>alert('2')</script>";      
-    $login_check = mysqli_num_rows($sql);
-    print "<script>alert('3')</script>";      
-    if($login_check > 0){
-    print "<script>alert('4')</script>";          
-        while($row = mysqli_fetch_array($sql)){
-            foreach ($row as $key => $val){
-                $$key = stripslashes( $val);
-            }
-  
-            $_SESSION['mail'] = $mail;
-            print "<script>alert('BIENVENIDO');</script>";            
-            print "<script> window.location='../index.php';</script>";
 
+if($sql = mysqli_query($mysqli, "SELECT email, password FROM user WHERE email = '$mail' AND password = '$pass' ")){     
+    $login_check = mysqli_num_rows($sql);    
+        if($login_check >=  1){        
+            while($row = mysqli_fetch_array($sql)){
+                foreach ($row as $key => $val){
+                    $$key = stripslashes( $val);
+                }
+                $_SESSION['mail'] = $mail;
+                print "<script>alert('BIENVENIDO');</script>";                       
+                print "<script> window.location='../index2.php';</script>";
+            }
+        }else{
+            print "<script>alert('Usuario o Contraseña Incorrectos 2');</script>";
+            print "<script> window.location='../index.php';</script>";
         }
-    }else{
-        print "<script>alert('Usuario o Contraseña Incorrectos 2');</script>";
-        print "<script> window.location='../index.php';</script>";
-    }
 }
 mysqli_close($mysqli);
 ?>
---
+++ index2.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="utf-8">
+        <title>CompuStore</title>
+        <meta name="viewport" content="width=device-width">
+        <script src="libs/mdl/material.js" async></script>
+        <link rel="stylesheet" href="css/base.css"/>
+        <link rel="stylesheet" href="css/registro.css">
+        <link rel="stylesheet" href="libs/mdl/material.css">
+        <script src="libs/angular/angular.min.js"></script>
+        <script src="libs/angular/angular-route.min.js"></script>
+        <script src="app.js"></script>
+        <script src="controladores/inicio.js"></script>
+        <script src="controladores/navegacionControl.js"></script>
+        <script src="controladores/confirmacion.js"></script>
+        <script src="servicios/carritoDeCompras.js"></script>
+    </head>
+    <body ng-app='navegacion'>
+        <header ng-controller="navegacionControl">
+            <nav>
+                <ul>
+                    <li class=""> <a href="actions/cerrar_sesion.php" class="cerrar"> Cerrar sesión </a></li>
+                </ul>
+            </nav>
+        </header>
+        <div class="main">
+            <div class="container" ng-view="ng-view"></div>
+        </div>
+    </body>
